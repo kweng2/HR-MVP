c2769982000d2b347423cb4125aac0d2c5211764
kweng2 <kweng2@gmail.com>
2016-01-05 15:26:58 -0800

    Styled food to be orange, added level selector, added arrow key controls

--- client/app.js
+++ client/app.js
@@ -7,6 +7,7 @@ Snake.controller('gameCtrl', function ($scope, $window, $interval, $http) {
   $scope.samePlayer = false;
   $scope.gameInstruct = false;
   $scope.inGame = false;
+  $scope.level = 50;
 
   // helper function to query database for previous players
   var getPlayers = function ($http, $scope) {
@@ -25,10 +26,6 @@ Snake.controller('gameCtrl', function ($scope, $window, $interval, $http) {
       method: 'POST',
       url: '/api/users',
       data: {name: $scope.playerName}
-      //STUFF HERE
-      /////////////////////////////////////////////////
-      //////////// substitute new high score //////////
-      /////////////////////////////////////////////////
     })
     .then(function (resp) {
       return resp.data;
@@ -58,11 +55,8 @@ Snake.controller('gameCtrl', function ($scope, $window, $interval, $http) {
     // try to add a new user
     var addedUser = addUsers($http, $scope);
     addedUser.then(function(res){
-      // debugger;
+
       if(res.error === 'Player already exists') {   // Error, existing player
-        /////////////////////////////////////////////////
-        //////////// Warn, existing player  /////////////
-        /////////////////////////////////////////////////
         $scope.samePlayer = true;
         $scope.currentHighScore = res.player.highScore;
         $scope.currentName = res.player.name;
@@ -80,15 +74,16 @@ Snake.controller('gameCtrl', function ($scope, $window, $interval, $http) {
 
   $scope.keyPress = function ($event) {
     var newDir = null;
-    if ($event.which === 119) {         // if the key is w, for up
+    // console.log($event.keyCode);
+    if ($event.keyCode === 87 || $event.keyCode === 38) {         // if the key is w, for up
       newDir = 1;
-    } else if ($event.which === 100) {  // key is d for right
+    } else if ($event.keyCode === 68 || $event.keyCode === 39) {  // key is d for right
       newDir = 2;
-    } else if ($event.which === 115) {  // key is s for down
+    } else if ($event.keyCode === 83 || $event.keyCode === 40) {  // key is s for down
       newDir = -1;
-    } else if ($event.which === 97) {   // key is a for left
+    } else if ($event.keyCode === 65 || $event.keyCode === 37) {   // key is a for left
       newDir = -2;
-    } else if ($event.which === 107) {  // key is k
+    } else if ($event.keyCode === 75) {  // key is k
       if(!this.inGame && !this.showInput && !this.samePlayer) { // K to start game
         this.gameInstruct = false;
         this.startGame();
@@ -98,7 +93,7 @@ Snake.controller('gameCtrl', function ($scope, $window, $interval, $http) {
         this.showCover = false;
         this.gameInstruct = true;
       }
-    } else if ($event.which === 106) {  // key is j to change player
+    } else if ($event.keyCode === 74) {  // key is j to change player
       if (!this.showInput && !this.samePlayer && !this.inGame) {  // game end
         this.gameInstruct = false;
         this.showInput = true;
@@ -117,6 +112,9 @@ Snake.controller('gameCtrl', function ($scope, $window, $interval, $http) {
   $scope.startGame = function () {
     // initialize the game
     $scope.gameState = init();
+
+    // set game speed to the user selected level
+    $scope.gameState.speed = Number($scope.level);
 
     // check each frame on an interval
     $scope.eachFrame = $interval(function() {
@@ -142,15 +140,13 @@ Snake.controller('gameCtrl', function ($scope, $window, $interval, $http) {
     }
     // check collision with screen edge and self
     var collision = collideWithEdge(gameState.head, gameState.gameWindow) || collideWithSelf(gameState.head, gameState.body);
+
     // if there are collisions, end the loop
     if (collision) {
       $interval.cancel($scope.eachFrame);
       $scope.gameInstruct = true;
       $scope.inGame = false;
-      // $scope.showInput = true;
-      ///////////////////////////////////////////////////////////////
-      ////////////////// DO SOMETHING ///////////////////////////////
-      ///////////////////////////////////////////////////////////////
+
       // check to see if current score is greater than this player's highscore
       if ($scope.gameState.body.length > $scope.currentHighScore) {
         // set player's highscore to be the current score
@@ -163,19 +159,17 @@ Snake.controller('gameCtrl', function ($scope, $window, $interval, $http) {
           });
         });
       }
-    } else {
-      ///////////////////////////////////////////////////////////////
-      ////////////////// DO SOMETHING ///////////////////////////////
-      ///////////////////////////////////////////////////////////////
-      // update display
+    } else {  // no collisions, update display
       updateGameBoard($scope);
     }
-    // window.gameState = gameState;
+    // update game state
     $scope.gameState = gameState;
   };
 
   // draw the game board
   var updateGameBoard = function ($scope) {
+    $scope.canvasCtx.fillStyle = 'black';
+
     $scope.canvasCtx.clearRect(0,0,$scope.gameState.gameWindow.width,$scope.gameState.gameWindow.height);
     var w = $scope.gameState.stepSize;
     var h = w;
@@ -185,6 +179,7 @@ Snake.controller('gameCtrl', function ($scope, $window, $interval, $http) {
 
       $scope.canvasCtx.fillRect(x, y, w, h);
     }
+    $scope.canvasCtx.fillStyle = '#FF4500';
     $scope.canvasCtx.fillRect($scope.gameState.food.x, $scope.gameState.food.y, w, h);
   };
 
